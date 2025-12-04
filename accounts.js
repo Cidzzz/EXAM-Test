69ac14adeab3926962fa6d36ca9b978d3fcf60e8
Cidzzz <dwiagung2225@gmail.com>
2025-12-04 21:48:34 +0700

    Update accounts.js

--- accounts.js
+++ accounts.js
@@ -10,8 +10,14 @@ const userAccounts = [
         nama_tampilan: 'Kirani XI TJAT 2'
     },
     {
+        username: 'kirani',
+        password: '38',
+        nama_tampilan: 'Kirani XI TJAT 2'
+    },
+    {
         username: 'siswa',
         password: 'belajar',
         nama_tampilan: 'Siswa Rajin'
     }
+
 ];
